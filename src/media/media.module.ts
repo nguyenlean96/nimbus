61f5e06a2a7db5f30a5368038deb6134a9b35f5f
horizon-team-inc <horizon.team.toronto@gmail.com>
2025-05-02 14:06:31 -0400

    feat: add StorageModule to MediaModule imports for enhanced storage capabilities

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -3,9 +3,10 @@ import { MediaService } from './media.service';
 import { MediaController } from './media.controller';
 import { DatabaseModule } from '@/database/database.module';
 import { mediaProviders } from './media.providers';
+import { StorageModule } from '@/storage/storage.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, StorageModule],
   controllers: [MediaController],
   providers: [...mediaProviders, MediaService],
 })
