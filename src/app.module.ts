2e206f77e4db4a01265881d9320799593d31614f
horizon-team-inc <horizon.team.toronto@gmail.com>
2025-05-02 14:06:40 -0400

    feat: add ServeStaticModule to serve static files from storage directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MediaModule } from './media/media.module';
 import { databaseProviders } from '@/database/database.providers';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MediaModule
+    MediaModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'storage', 'public'),
+      serveRoot: '/storage',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, ...databaseProviders],
