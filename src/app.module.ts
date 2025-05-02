e450d4e1512de878ec207b62bff90f0042e058dc
horizon-team-inc <horizon.team.toronto@gmail.com>
2025-05-01 23:29:14 -0400

    feat: add @nestjs/config dependency and initialize ConfigModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MediaModule } from './media/media.module';
 import { databaseProviders } from '@/database/database.providers';
 
 @Module({
-  imports: [MediaModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MediaModule
+  ],
   controllers: [AppController],
   providers: [AppService, ...databaseProviders],
   exports: [...databaseProviders],
